e5082ea5f3345adbd1434c1db34ad48d769b6356
JustinHaase <74632537+JustinHaase@users.noreply.github.com>
2021-02-09 14:36:17 -0500

    added .js and .html information

--- rest/index.js
+++ rest/index.js
@@ -1,11 +1,22 @@
 const express = require('express')
+const axios = require ('axios')
+const cors = require("cors")
 const app = express()
-const port = 3000
+const port = 4000
 
-app.get('/', (req, res) => {
+app.use(cors())
+
+app.get('/pokemon/random', async (req, res) => {
+let pokemonResponse = axios.get("https://pokeapi.co/api/v2/pokemon/ditto")
+  let pokemonJson = await PokemonResponse.json();
+
+  console.log(pokemonJson)
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+xhr.open("GET","localhost:4000/pokemon/random" )
+xhr.send();
