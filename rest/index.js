5dc76f28938a9a42bd1e30a31410ef2029efb3b8
JustinHaase <74632537+JustinHaase@users.noreply.github.com>
2021-02-11 17:07:02 -0500

    Revert "Display One pokemon. (#1)"
    This reverts commit 74f06cf0d5eda337224210c4ff6479daa3e9e0b4.

--- rest/index.js
+++ rest/index.js
@@ -6,52 +6,11 @@ const port = 4000
 
 app.use(cors())
 
-//app.use('/pokemon', express.static('web'))
-
-let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-
-
 app.get('/pokemon/random', async (req, res) => {
-let pokemonResponse = await axios.get("https://pokeapi.co/api/v2/pokemon/25");
+let pokemonResponse = axios.get("https://pokeapi.co/api/v2/pokemon/ditto")
+  let pokemonJson = await PokemonResponse.json();
 
-//not working as an object on the web side. Getting a string   
- /*let createPoke = {
-   id: pokemonResponse.data.id,
-   name: pokemonResponse.data.name,
-   image: pokemonResponse.data.sprites.front_default
- }
- */
-    //console.log(pokemonResponse.data.message)
-        //res.send(pokemonResponse.data.message)
-        //.sprites.front_default
-  
-       res.send(pokemonResponse.data.sprites.front_default)
-      //res.send(createPoke)
-      //console.log(typeof(createPoke))
-       //res.send(pokemonResponse.data)
-     })
-
-
-
-app.listen(port, () => {
-  console.log(`Poke app listening at http://localhost: ${port}`)
-})
-
-
-
-
-
-
-
-
-/*
-app.get('/pokemon/random', async (req, res) => {
-let pokemonResponse = axios.get("https://pokeapi.co/api/v2/pokemon")
-  let pokemonJson = await pokemonResponse.data;
- 
-  //https://dog.ceo/api/breeds/image/random
-//https://pokeapi.co/api/v2/pokemon/ditto
-  console.log(pokemonJson);
+  console.log(pokemonJson)
   res.send("Pokemon!")
 })
 
@@ -59,9 +18,5 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-xhr.open("GET","http://localhost:4000" )
-
-//xhr.open("GET","localhost:4000/pokemon/random" )
+xhr.open("GET","localhost:4000/pokemon/random" )
 xhr.send();
-
-*/
