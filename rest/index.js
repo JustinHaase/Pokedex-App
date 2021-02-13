0f582e68d4fdd87dd93ed048f1f3f9a160538696
Kayanr <kayan.reid@gmail.com>
2021-02-13 13:40:31 -0500

    modify rest\index.js, web\index.js, web\index.html,
    modify rest\index.js to use fetch instead of axios. Also sent 150 pokemon details  to localhost:4000//pokemon/random. Modify card div tags to include ids to make the divs easier to target.

--- rest/index.js
+++ rest/index.js
@@ -1,67 +1,32 @@
 const express = require('express')
-const axios = require ('axios')
+//const axios = require ('axios')
 const cors = require("cors")
 const app = express()
 const port = 4000
 
 app.use(cors())
+const fetch = require("node-fetch");
 
-//app.use('/pokemon', express.static('web'))
+const promises = [];
 
-// let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-
-
-app.get('/pokemon/random', async (req, res) => {
-let pokemonResponse = await axios.get("https://pokeapi.co/api/v2/pokemon/25");
-
-//not working as an object on the web side. Getting a string   
- /*let createPoke = {
-   id: pokemonResponse.data.id,
-   name: pokemonResponse.data.name,
-   image: pokemonResponse.data.sprites.front_default
- }
- */
-    //console.log(pokemonResponse.data.message)
-        //res.send(pokemonResponse.data.message)
-        //.sprites.front_default
-  
-       res.send(pokemonResponse.data.sprites.front_default)
-      //res.send(createPoke)
-      //console.log(typeof(createPoke))
-       //res.send(pokemonResponse.data)
-     })
-
-
+app.get('/pokemon/random', (req, res) => {
+    for (let i = 1; i <= 150; i++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then((res) => res.json()));
+    }
+    Promise.all(promises).then((results) => {
+        const pokemonResponse = results.map((result) => ({
+            name: result.name,
+            image: result.sprites['front_default'],
+            type: result.types.map((type) => type.type.name).join(', '),
+            id: result.id
+        }));
+        
+        res.send(pokemonResponse);
+    });
+});
 
 app.listen(port, () => {
   console.log(`Poke app listening at http://localhost: ${port}`)
 })
 
-
-
-
-
-
-
-
-/*
-app.get('/pokemon/random', async (req, res) => {
-let pokemonResponse = axios.get("https://pokeapi.co/api/v2/pokemon")
-  let pokemonJson = await pokemonResponse.data;
- 
-  //https://dog.ceo/api/breeds/image/random
-//https://pokeapi.co/api/v2/pokemon/ditto
-  console.log(pokemonJson);
-  res.send("Pokemon!")
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
-
-xhr.open("GET","http://localhost:4000" )
-
-//xhr.open("GET","localhost:4000/pokemon/random" )
-xhr.send();
-
-*/
--- web/index.js
+++ web/index.js
@@ -1,16 +1,22 @@
 //All front end JavaScript should go here
 
 let xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function(){
+xhr.onreadystatechange =  function () {
+  if ((this.readyState === 4) & (this.status === 200)) {
+           
+    let myObject =  JSON.parse(this.responseText);
 
-if(this.readyState === 4 & this.status === 200){
-    document.getElementsByTagName("img")[0].src = this.responseText;
-    //document.querySelector(".pokecard").textContent = `${this.responseText[0]}`
-    //`${typeof(this.responseText)}`;
-    //console.log(this.responseText.image)
-}
-
-
-}
-xhr.open('GET', 'http://localhost:4000/pokemon/random');
+    for(let i = 0; i<12 ; i++){
+        document.getElementsByTagName("img")[i+2].src = myObject[i].image;
+    }
+    
+    for(let i = 0; i<13 ; i++){
+    document.querySelector(`#pname${i+1}`).textContent = `Name: ${myObject[i].name} Id number:${myObject[i].id}`;
+    document.querySelector(`#ptype${i+1}`).textContent = `Type: ${myObject[i].type}`;
+    }   
+     
+  
+  }
+};
+xhr.open("GET", "http://localhost:4000/pokemon/random");
 xhr.send();
