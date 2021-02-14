21987f7d0f083efbc1e294ac2c8ac683b149d00f
JustinHaase <74632537+JustinHaase@users.noreply.github.com>
2021-02-13 20:51:31 -0500

    fixed mistakes I made when approving pull request, continued styling work

--- web/index.js
+++ web/index.js
@@ -14,18 +14,31 @@ function myFunction() {
   }
 }
 
-
 let xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function(){
-
-if(this.readyState === 4 & this.status === 200){
-    document.getElementsByTagName("img")[2].src = this.responseText;
-    //document.querySelector(".pokecard").textContent = `${this.responseText[0]}`
-    //`${typeof(this.responseText)}`;
-    //console.log(this.responseText.image)
-}
-
-
-}
-xhr.open('GET', 'http://localhost:4000/pokemon/random');
+xhr.onreadystatechange =  function () {
+  if ((this.readyState === 4) & (this.status === 200)) {
+           
+    let myObject =  JSON.parse(this.responseText);
+    for(let i = 0; i<12 ; i++){
+        document.getElementsByTagName("img")[i+2].src = myObject[i].image;
+    }
+    
+    for(let i = 0; i<13 ; i++){
+    document.querySelector(`#pname${i+1}`).textContent = `Name: ${myObject[i].name} Id number:${myObject[i].id}`;
+    document.querySelector(`#ptype${i+1}`).textContent = `Type: ${myObject[i].type}`;
+  
+    }   
+     
+  
+  }
+};
+xhr.open("GET", "http://localhost:4000/pokemon/random");
 xhr.send();
+
+$('pokecard').has('div.grass').css('background-color', 'green');
+
+//create dynamic styling effect in each card based on data pulled from api
+
+//if type = 
+
+//document.getElementById("ptype").style["padding-top"] = "10px";
