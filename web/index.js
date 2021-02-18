ef6345c4b0cf8acc081b84e1cd285bae60a084fe
JustinHaase <74632537+JustinHaase@users.noreply.github.com>
2021-02-18 04:06:55 -0500

    enhanced stying, worked on asigning color data to pokecard, added media

--- web/index.js
+++ web/index.js
@@ -14,19 +14,48 @@ function myFunction() {
   }
 }
 
+
+const colorMap= {'fire':'type__fire', 'grass':'type__grass', 'water':'type__water'};
+const colorMapHex = {'fire':'#FDDFDF', 'grass':'#DEFDE0', 'electric':'#FCF7DE', 'water':'#DEF3FD','ground':'#f7d3aa','rock':'D5D5D4','fairy':'#FCEAFF','poison':'#98D7A5','bug':'#F8D7A5','dragon':'#97B3E6', 'psychic':'#EAEDA1', 'flying':'#dbb2f7', 'fighting':'#E6E0D4', 'normal':'#F5F5F5'};
+
+//  const types = [];
+//  const element;
+//  element.style.background = `Linear gradient: ${types.map( type => colorMapHex[type]).join(" ")}`
+
+//button click music play function
+// function playMusic(){
+//   var music = new Audio('Theme.mid');
+//   music.play();
+//   }
+
+var music = document.getElementById('music');
+
+function playAudio() {
+	if (music.paused){
+		music.play();
+		pButton.className = "";
+		pButton.className = "pause";
+	} else {
+		music.pause();
+		pButton.className = "";
+		pButton.className = "play";
+	}
+}
+
 let xhr = new XMLHttpRequest();
 xhr.onreadystatechange =  function () {
   if ((this.readyState === 4) & (this.status === 200)) {
            
     let myObject =  JSON.parse(this.responseText);
     for(let i = 0; i<12 ; i++){
+
         document.getElementsByTagName("img")[i+2].src = myObject[i].image;
     }
     
-    for(let i = 0; i<13 ; i++){
+    for(let i = 0; i<12 ; i++){
     document.querySelector(`#pname${i+1}`).textContent = `${myObject[i].name} id:${myObject[i].id}`;
     document.querySelector(`#ptype${i+1}`).textContent = `${myObject[i].type}`;
-  
+    document.querySelectorAll('.pokecard')[i].classList.add(colorMap[myObject[i].type])
     }   
      
   
